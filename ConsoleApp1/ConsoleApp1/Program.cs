cbf9a59a4ad3fb6c5a7cea3902fd60cafd9e6e53
faberlissss <maria.simochka@gmail.com>
2026-04-27 23:17:11 +0300

    Program code added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter the first number: ");
+            string firstNum = Console.ReadLine();
+            int.TryParse(firstNum, out int firstNumParse);
+
+            Console.WriteLine("Enter the second number: ");
+            string secondNum = Console.ReadLine();
+            int.TryParse(secondNum, out int secondNumParse);
+
+            int result = firstNumParse / secondNumParse;
+            Console.WriteLine($"Result: {result}"); 
+            
+        }
+        catch(DivideByZeroException)
+        {
+            Console.WriteLine("An exception occurred!");
+        }
+    }
+}
